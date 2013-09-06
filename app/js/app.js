180917be50948c30054e5a88abc03a6312840017
FirstKlas <klas@firstklas.se>
2013-09-06 13:54:08 +0200

    Gnägg

--- app/js/app.js
+++ app/js/app.js
@@ -7,5 +7,6 @@ angular.module('myApp', ['myApp.controllers', 'firebase']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/ekonomi', {templateUrl: 'partials/ekonomi.html', controller: 'ekonomiRapportCtrl'}).
+      when('/arkiv', {templateUrl: 'partials/arkiv.html', controller: 'arkivCtrl'}).
       otherwise({redirectTo: '/ekonomi'});
 }]);
---
+++ app/js/arkivCtrl.js
@@ -0,0 +1,3 @@
+angular.module('myApp.controllers').controller('arkivCtrl', ['$scope', '$window', 'Firebase', 'angularFireCollection', function ($scope, $window, Firebase, angularFireCollection) {
+
+}]);
