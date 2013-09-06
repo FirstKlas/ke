7a668eaba91b916e754f9e522bfa2f06929c50ab
FirstKlas <klas@firstklas.se>
2013-09-06 13:12:09 +0200

    Gnägg

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
 /* App Module */
+angular.module('myApp.controllers', []);
 
-angular.module('myApp', ['firebase']).
+angular.module('myApp', ['myApp.controllers', 'firebase']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/ekonomi', {templateUrl: 'partials/ekonomi.html', controller: ekonomiCtrl}).
+      when('/ekonomi', {templateUrl: 'partials/ekonomi.html', controller: ekonomiRapportCtrl}).
       otherwise({redirectTo: '/ekonomi'});
 }]);
