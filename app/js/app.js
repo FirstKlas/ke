99f788b9242bdd689042629789bf9c12a3cc3f23
FirstKlas <klas@firstklas.se>
2013-09-09 16:31:18 +0200

    Gnägg

--- app/js/app.js
+++ app/js/app.js
@@ -3,11 +3,11 @@
 /* App Module */
 angular.module('myApp.controllers', []);
 
-angular.module('myApp', ['myApp.controllers', 'Firebase']).
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/ekonomi', {templateUrl: 'partials/ekonomi.html', controller: 'ekonomiRapportCtrl'}).
-      when('/arkiv', {templateUrl: 'partials/arkiv.html', controller: 'arkivCtrl'}).
-      when('/login', {templateUrl: 'partials/login.html', controller: 'loginCtrl'}).
-      otherwise({redirectTo: '/login'});
-}]);
+angular.module('myApp', ['myApp.controllers', 'firebase']).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+            when('/ekonomi', {templateUrl: 'partials/ekonomi.html', controller: 'ekonomiRapportCtrl'}).
+            when('/arkiv', {templateUrl: 'partials/arkiv.html', controller: 'arkivCtrl'}).
+            when('/login', {templateUrl: 'partials/login.html', controller: 'loginCtrl'}).
+            otherwise({redirectTo: '/login'});
+    }]);
--- app/js/loginCtrl.js
+++ app/js/loginCtrl.js
@@ -1,4 +1,11 @@
 angular.module('myApp.controllers').controller('loginCtrl', ['$scope', 'angularFireAuth', 'Firebase', function ($scope, angularFireAuth, Firebase) {
     var ref = new Firebase("https://ke.firebaseio.com/");
     angularFireAuth.initialize(ref, {scope: $scope, name: "user"});
+
+    $scope.login = function() {
+        angularFireAuth.login("facebook");
+    };
+    $scope.logout = function() {
+        angularFireAuth.logout();
+    };
 }]);
